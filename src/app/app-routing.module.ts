19f524fdb6daef31f09d24994c708060b5b1ac5c
yulia <petukhova.yulia99@gmail.com>
2024-11-22 16:35:14 +0400

    base routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
+    { path: 'main', loadChildren: () => import('./components/main/main.component').then(m => m.MainComponent) },
+    { path: 'about', loadChildren: () => import('./components/about/about.component').then(m => m.AboutComponent) },
+    { path: 'repair-services', loadChildren: () => import('./components/repair-services/repair-services.component').then(m => m.RepairServicesComponent) },
+    { path: 'reviews', loadChildren: () => import('./components/reviews/reviews.component').then(m => m.ReviewsComponent) },
+    { path: '**', redirectTo: '/main' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
